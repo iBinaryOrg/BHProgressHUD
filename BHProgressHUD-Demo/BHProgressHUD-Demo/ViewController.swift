c307fc99d2256fe75dca616f84b679203aa049f4
Raykle <raykle.up@gmail.com>
2016-06-01 20:23:30 +0800

    Add Demo Project

---
+++ BHProgressHUD-Demo/BHProgressHUD-Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BHProgressHUD-Demo
+//
+//  Created by Raykle on 16/6/1.
+//  Copyright © 2016年 iBinaryOrg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
